859cb8432ceaf36ddb2d726af64553b2823dc623
Grace Xu <gracexucs@gmail.com>
2023-07-01 21:07:47 -0500

    Set up spring, vue, and related dependencies

---
+++ focusflockSpring/src/test/kotlin/com/focusflock/FocusflockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.focusflock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FocusflockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
